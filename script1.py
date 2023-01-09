5e988ab81c7e9156e308e265c95741b979cc899c
sandracascantem <99333138+sandracascantem@users.noreply.github.com>
2023-01-09 18:07:29 +0100

    Update script1.py

--- script1.py
+++ script1.py
@@ -5,13 +5,13 @@ from subprocess import call
 
 #Funcion arch para reemplazar dentro de un archivo
 def arch(fi, rep1, rep2):
-	fin = open('practica_creativa2/bookinfo/src/productpage/' + fi, 'r')
+	fin = open('/practica_creativa2/bookinfo/src/productpage/' + fi, 'r')
 
 	with fin as file:
 		x= file.read()
 	fin.close()
 
-	fin = open('practica_creativa2/bookinfo/src/productpage/' + fi, 'w')
+	fin = open('/practica_creativa2/bookinfo/src/productpage/' + fi, 'w')
 	with fin as file:
 		x= x.replace(rep1, rep2)
 		fin.write(x)
@@ -28,7 +28,7 @@ arch('requirements.txt', 'urllib3==1.26.5', 'urllib3==1.26.13')
 #Instalamos pip sudo apt-get install python-pip
 subprocess.run(["sudo", "apt-get", "install", "python3-pip"])
 #Instalamos dependencias de requirements.txt con pip
-subprocess.run(["pip3", "install", '-r', 'practica_creativa2/bookinfo/src/productpage/requirements.txt'])
+subprocess.run(["pip3", "install", '-r', '/practica_creativa2/bookinfo/src/productpage/requirements.txt'])
 
 
 #Creamos la variable de entorno (con nuestro num de grupo, 35)
@@ -45,4 +45,4 @@ puerto = input("Introduce un puerto: \n")
 arch('productpage_monolith.py', '9080', puerto)
 
 #Llamamos a productpage_monolith.py para crear la aplicacion (como si fuese un monolito)
-subprocess.run(["python3", "practica_creativa2/bookinfo/src/productpage/productpage_monolith.py", puerto])
+subprocess.run(["python3", "/practica_creativa2/bookinfo/src/productpage/productpage_monolith.py", puerto])
