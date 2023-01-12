1d2c0fcb01e3c6b851b057639ee5c291b6cdc6b8
sandracascantem <99333138+sandracascantem@users.noreply.github.com>
2023-01-12 14:06:22 +0100

    Update script0.py

--- script0.py
+++ script0.py
@@ -17,11 +17,6 @@ def arch(fi, rep1, rep2):
 		fin.write(x)
 	fin.close()
 
-#Clonamos la carpeta practica_creativa2 del github
-subprocess.run(["git", "clone", "https://github.com/CDPS-ETSIT/practica_creativa2"])
-#Cambiamos de directorio (a partir de ahora se trabaja dentro de la carpeta practica_creativa2)
-#os.chdir('/home/sandracascantemoran/scriptsCDPS')
-
 #Modificamos el fichero requirements.txt
 arch('requirements.txt', 'urllib3==1.26.5', 'urllib3')
 arch('requirements.txt', 'chardet==3.0.4', 'chardet')
