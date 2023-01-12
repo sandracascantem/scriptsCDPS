e951d51dbce7db9d2e20e30cc30e6133ced406ae
sandracascantem <99333138+sandracascantem@users.noreply.github.com>
2023-01-12 13:10:58 +0100

    Update script2.py

--- script2.py
+++ script2.py
@@ -15,7 +15,7 @@ subprocess.run(["sudo", "apt", "install", "docker-compose"])
 
 #Creamos Dockerfile
 doc = open("./Dockerfile", "w")
-doc.write("FROM python 3.0\n")
+doc.write("FROM python:3.7\n")
 doc.write("#Variable entorno al contenedor\nENV GROUP_NUMBER '35'\n")
 doc.write("#Abrimos el puerto 9080\nEXPOSE 9080\n")
 doc.write("#Lanza la aplicacion\nCMD ['python3', './practica_creativa2/bookinfo/src/productpage/productpage_monolith.py', '9080']\n")
@@ -23,7 +23,7 @@ doc.write("#Lanza la aplicacion\nCMD ['python3', './practica_creativa2/bookinfo/
 #Creamos imagen
 subprocess.run(["sudo", "docker", "build", "-t", "35/product-page", "."])
 
-print(GROUP_NUMBER)
+print($GROUP_NUMBER)
 #Modificamos productpage.html para cambiar el titulo de la app por la variable de entorno
 #arch('templates/productpage.html', 'Simple Bookstore App', $GROUP_NUMBER)
 
