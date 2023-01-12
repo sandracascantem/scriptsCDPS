ab5448289668bd5704b2d796c69073788cb1cb30
sandracascantem <99333138+sandracascantem@users.noreply.github.com>
2023-01-12 13:07:23 +0100

    Update script2.py

--- script2.py
+++ script2.py
@@ -21,7 +21,7 @@ doc.write("#Abrimos el puerto 9080\nEXPOSE 9080\n")
 doc.write("#Lanza la aplicacion\nCMD ['python3', './practica_creativa2/bookinfo/src/productpage/productpage_monolith.py', '9080']\n")
 
 #Creamos imagen
-subprocess.run(["docker", "build", "-t", "35/product-page", "."])
+subprocess.run(["sudo", "docker", "build", "-t", "35/product-page", "."])
 
 print(GROUP_NUMBER)
 #Modificamos productpage.html para cambiar el titulo de la app por la variable de entorno
