8dc32d8c76a64d5e927ea8753cb70f4d8e82c15f
sandracascantem <99333138+sandracascantem@users.noreply.github.com>
2023-01-23 14:50:40 +0100

    Update script3.py

--- part3/script3.py
+++ part3/script3.py
@@ -3,21 +3,10 @@
 import os, sys, subprocess
 from subprocess import call
 
-#Funcion arch para reemplazar dentro de un archivo
-def arch(fi, rep1, rep2):
-	fin = open('./practica_creativa2/bookinfo/src/productpage/' + fi, 'r')
-
-	with fin as file:
-		x= file.read()
-	fin.close()
-
-	fin = open('./practica_creativa2/bookinfo/src/productpage/' + fi, 'w')
-	with fin as file:
-		x= x.replace(rep1, rep2)
-		fin.write(x)
-	fin.close()
-
 #Clonamos la carpeta practica_creativa2 del github
 subprocess.run(["git", "clone", "https://github.com/CDPS-ETSIT/practica_creativa2"])
-#Cambiamos de directorio (a partir de ahora se trabaja dentro de la carpeta practica_creativa2)
-#os.chdir('/home/sandracascantemoran/scriptsCDPS')
+
+#Cambiamos de directorio (a partir de ahora se trabaja dentro de la ruta src/reviews/reviews-wlpcfg)
+os.chdir('./practica_creativa2/bookinfo/src/reviews/reviews-wlpcfg')
+
+call(["docker run --rm -u root -v " + r' " ' + "$(pwd)" + r' " ' + ":/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build"], shell=True)
