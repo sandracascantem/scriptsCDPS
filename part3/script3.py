c5e90cd42c75a537d375d4c62e8a70d83f00bfee
sandracascantem <99333138+sandracascantem@users.noreply.github.com>
2023-01-23 14:21:31 +0100

    Update script3.py

--- part3/script3.py
+++ part3/script3.py
@@ -1,1 +1,20 @@
+#!/usr/bin/python3
 
+import os, sys, subprocess
+from subprocess import call
+
+#Funcion arch para reemplazar dentro de un archivo
+def arch(fi, rep1, rep2):
+	fin = open('./practica_creativa2/bookinfo/src/productpage/' + fi, 'r')
+
+	with fin as file:
+		x= file.read()
+	fin.close()
+
+	fin = open('./practica_creativa2/bookinfo/src/productpage/' + fi, 'w')
+	with fin as file:
+		x= x.replace(rep1, rep2)
+		fin.write(x)
+	fin.close()
+
+#Modificamos el fichero requirements.txt
