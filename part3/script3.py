f14f507a23646d47aa4dc464cce4d0afc084d30b
sandracascantem <99333138+sandracascantem@users.noreply.github.com>
2023-01-23 14:52:40 +0100

    Update script3.py

--- part3/script3.py
+++ part3/script3.py
@@ -9,4 +9,5 @@ subprocess.run(["git", "clone", "https://github.com/CDPS-ETSIT/practica_creativa
 #Cambiamos de directorio (a partir de ahora se trabaja dentro de la ruta src/reviews/reviews-wlpcfg)
 os.chdir('./practica_creativa2/bookinfo/src/reviews/reviews-wlpcfg')
 
+#compilar y empaquetar ficheros necesarios
 call(["docker run --rm -u root -v " + r' " ' + "$(pwd)" + r' " ' + ":/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build"], shell=True)
