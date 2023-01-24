397d582241b6413e18d37d86d17ed907c298fd25
sandracascantem <99333138+sandracascantem@users.noreply.github.com>
2023-01-24 19:19:30 +0100

    Update script3.py

--- part3/script3.py
+++ part3/script3.py
@@ -12,9 +12,10 @@ run(["cp", "practica_creativa2/bookinfo/src/details/.DS_Store", "./details/."])
 run(["cp", "practica_creativa2/bookinfo/src/ratings/ratings.js", "./ratings/."])
 run(["cp", "practica_creativa2/bookinfo/src/ratings/package.json", "./ratings/."])
 run(["cp", "practica_creativa2/bookinfo/src/ratings/.DS_Store", "./ratings/."])
+run(["cp", "-R", "practica_creativa2/bookinfo/src/reviews", "."])
 
 #Cambiamos de directorio (a partir de ahora se trabaja dentro de la ruta src/reviews/reviews-wlpcfg)
-os.chdir('./practica_creativa2/bookinfo/src/reviews/reviews-wlpcfg')
+os.chdir('./reviews/reviews-wlpcfg')
 
 #compilar y empaquetar ficheros necesarios
 call(['sudo docker run --rm -u root -v "$(pwd)":/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build'], shell=True)
