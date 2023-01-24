2127fe758ef82bba16b3e413a40d1a118f1f4448
sandracascantem <99333138+sandracascantem@users.noreply.github.com>
2023-01-24 17:35:40 +0100

    Update script3.py

--- part3/script3.py
+++ part3/script3.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 
 import os, sys, subprocess
-from subprocess import call
+from subprocess import call, run
 
 #Clonamos la carpeta practica_creativa2 del github
-subprocess.run(["git", "clone", "https://github.com/CDPS-ETSIT/practica_creativa2"])
+run(["git", "clone", "https://github.com/CDPS-ETSIT/practica_creativa2"])
+
+#Copiamos ficheros
+run(["cp", "practica_creativa2/bookinfo/src/details/details.rb", "./details/."])
+run(["cp", "practica_creativa2/bookinfo/src/ratings/ratings.js", "./ratings/."])
+run(["cp", "practica_creativa2/bookinfo/src/ratings/package.json", "./ratings/."])
 
 #Cambiamos de directorio (a partir de ahora se trabaja dentro de la ruta src/reviews/reviews-wlpcfg)
 os.chdir('./practica_creativa2/bookinfo/src/reviews/reviews-wlpcfg')
 
 #compilar y empaquetar ficheros necesarios
-call(["docker run --rm -u root -v " + r' " ' + "$(pwd)" + r' " ' + ":/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build"], shell=True)
+call(['sudo docker run --rm -u root -v "$(pwd)":/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build'], shell=True)
 
