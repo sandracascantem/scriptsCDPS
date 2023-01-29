f31c9bb66b0ce225ebc622084b8520815ca41164
sandracascantem <99333138+sandracascantem@users.noreply.github.com>
2023-01-29 20:28:04 +0100

    Create script3.py

---
+++ part4/script3.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import os, sys, subprocess
+from subprocess import call, run
+
+#Clonamos la carpeta practica_creativa2 del github
+run(["git", "clone", "https://github.com/CDPS-ETSIT/practica_creativa2"])
+
+#Copiamos ficheros
+run(["cp", "practica_creativa2/bookinfo/src/details/details.rb", "./details/."])
+run(["cp", "practica_creativa2/bookinfo/src/details/.DS_Store", "./details/."])
+run(["cp", "practica_creativa2/bookinfo/src/ratings/ratings.js", "./ratings/."])
+run(["cp", "practica_creativa2/bookinfo/src/ratings/package.json", "./ratings/."])
+run(["cp", "practica_creativa2/bookinfo/src/ratings/.DS_Store", "./ratings/."])
+
+#Cambiamos de directorio (a partir de ahora se trabaja dentro de la ruta src/reviews/reviews-wlpcfg)
+os.chdir('./practica_creativa2/bookinfo/src/reviews')
+
+#compilar y empaquetar ficheros necesarios
+call(['sudo docker run --rm -u root -v "$(pwd)":/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build'], shell=True)
