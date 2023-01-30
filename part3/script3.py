cdc44b94cd74aa30df85332fa177f6d78f5f710d
sandracascantem <99333138+sandracascantem@users.noreply.github.com>
2023-01-31 00:39:19 +0100

    Update script3.py

--- part3/script3.py
+++ part3/script3.py
@@ -1,33 +1,35 @@
 #!/usr/bin/python3
 
-import os, sys, subprocess
+import os, sys, subprocess, re
 from subprocess import call, run
 
 #Funcion para reemplazar dentro del docker-compose.yaml
 def dcompose_ver(fin, version):
-    options = {
-        "v1": {"SERVICE_VERSION": "v1", "ENABLE_RATINGS": "false", "STAR_COLOR": "black"},
-        "v2": {"SERVICE_VERSION": "v2", "ENABLE_RATINGS": "true", "STAR_COLOR": "black"},
-        "v3": {"SERVICE_VERSION": "v3", "ENABLE_RATINGS": "true", "STAR_COLOR": "red"}
-    }
+	options = {
+		"v1": {"SERVICE_VERSION": "v1", "ENABLE_RATINGS": "false", "STAR_COLOR": "black"},
+		"v2": {"SERVICE_VERSION": "v2", "ENABLE_RATINGS": "true", "STAR_COLOR": "black"},
+		"v3": {"SERVICE_VERSION": "v3", "ENABLE_RATINGS": "true", "STAR_COLOR": "red"}
+	}
+	
+	if version not in options:
+		print("No se ha seleccionado una versión válida para reviews (v1, v2, v3)\n")
+		exit()
+	
+	with open(fin, "r") as f:
+        	lines = f.readlines()
 
-    if version not in options:
-        print("No se ha seleccionado una versión de reviews válida (v1, v2, v3)")
-        exit()
+	new_lines = []
+    	for line in lines:
+		if "SERVICE_VERSION" in line:
+			line = re.sub(r"SERVICE_VERSION=\w+", f"SERVICE_VERSION={options[version]['SERVICE_VERSION']}", line)
+		if "ENABLE_RATINGS" in line:
+			line = re.sub(r"ENABLE_RATINGS=\w+", f"ENABLE_RATINGS={options[version]['ENABLE_RATINGS']}", line)
+		if "STAR_COLOR" in line:
+			line = re.sub(r"STAR_COLOR=\w+", f"STAR_COLOR={options[version]['STAR_COLOR']}", line)
+		new_lines.append(line)
 
-    with open(fin, "r") as f:
-        lines = f.readlines()
-        
-    new_lines = []
-    for line in lines:
-        for k, v in options[version].items():
-            if f"{k}=" in line:
-                line = line.replace(f"{k}=", f"{k}={v}")
-        new_lines.append(line)
-
-    with open(fin, "w") as f:
-        f.writelines(new_lines)
-
+	with open(fin, "w") as f:
+        	f.writelines(new_lines)
 		
 #Clonamos la carpeta practica_creativa2 del github
 run(["git", "clone", "https://github.com/CDPS-ETSIT/practica_creativa2"])
