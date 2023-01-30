fe98f570c41e2506a70adf7a25fcf4152bd8768e
sandracascantem <99333138+sandracascantem@users.noreply.github.com>
2023-01-30 21:19:11 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-#Funcion arch para reemplazar dentro de un archivo
+#Funcion arch para reemplazar dentro de un archivo de practica_creativa2
 def arch(fi, rep1, rep2):
 	fin = open('./practica_creativa2/bookinfo/src/productpage/' + fi, 'r')
 
@@ -9,6 +9,20 @@ def arch(fi, rep1, rep2):
 	fin.close()
 
 	fin = open('./practica_creativa2/bookinfo/src/productpage/' + fi, 'w')
+	with fin as file:
+		x= x.replace(rep1, rep2)
+		fin.write(x)
+	fin.close()
+
+#Funcion arch2 para reemplazar dentro del docker-compose.yaml de part3
+def arch(fi, rep1, rep2):
+	fin = open('./docker-compose.yaml' + fi, 'r')
+
+	with fin as file:
+		x= file.read()
+	fin.close()
+
+	fin = open('./docker-compose.yaml' + fi, 'w')
 	with fin as file:
 		x= x.replace(rep1, rep2)
 		fin.write(x)
