d32759e1240158ee5c795e58c56feb5917a37432
sandracascantem <99333138+sandracascantem@users.noreply.github.com>
2023-01-30 21:15:23 +0100

    Update script2.py

--- part2/script2.py
+++ part2/script2.py
@@ -2,20 +2,7 @@
 
 import os, sys, subprocess
 from subprocess import call
-
-#Funcion arch para reemplazar dentro de un archivo
-def arch(fi, rep1, rep2):
-	fin = open('./practica_creativa2/bookinfo/src/productpage/' + fi, 'r')
-
-	with fin as file:
-		x= file.read()
-	fin.close()
-
-	fin = open('./practica_creativa2/bookinfo/src/productpage/' + fi, 'w')
-	with fin as file:
-		x= x.replace(rep1, rep2)
-		fin.write(x)
-	fin.close()
+from functions import arch
 
 #Modificamos el fichero requirements.txt
 arch('requirements.txt', 'urllib3==1.26.5', 'urllib3')
