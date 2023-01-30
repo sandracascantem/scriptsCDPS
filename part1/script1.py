95b0c0ec1094783153e91602eb153dcd79bcf368
sandracascantem <99333138+sandracascantem@users.noreply.github.com>
2023-01-30 21:14:50 +0100

    Update script1.py

--- part1/script1.py
+++ part1/script1.py
@@ -2,20 +2,7 @@
 
 import os, sys, subprocess
 from subprocess import call
-
-#Funcion arch para reemplazar dentro de un archivo
-def arch(fi, rep1, rep2):
-	fin = open('./practica_creativa2/bookinfo/src/productpage/' + fi, 'r')
-
-	with fin as file:
-		x= file.read()
-	fin.close()
-
-	fin = open('./practica_creativa2/bookinfo/src/productpage/' + fi, 'w')
-	with fin as file:
-		x= x.replace(rep1, rep2)
-		fin.write(x)
-	fin.close()
+from functions import arch
 
 #Clonamos la carpeta practica_creativa2 del github
 subprocess.run(["git", "clone", "https://github.com/CDPS-ETSIT/practica_creativa2"])
@@ -36,7 +23,6 @@ subprocess.run(["pip3", "install", '-r', './practica_creativa2/bookinfo/src/prod
 os.environ['GROUP_NUMBER']= "35"
 #Extraemos la variable de entorno y guardarla aqui
 title = os.environ.get('GROUP_NUMBER')
-#print(title)
 
 #Modificamos productpage.html para cambiar el titulo de la app por la variable de entorno
 arch('templates/productpage.html', 'Simple Bookstore App', title)
