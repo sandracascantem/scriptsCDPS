ba24a4355475e6206baa0c9b4031ad60753aaffc
sandracascantem <99333138+sandracascantem@users.noreply.github.com>
2023-01-30 21:12:56 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,1 +1,15 @@
+#!/usr/bin/python3
 
+#Funcion arch para reemplazar dentro de un archivo
+def arch(fi, rep1, rep2):
+	fin = open('./practica_creativa2/bookinfo/src/productpage/' + fi, 'r')
+
+	with fin as file:
+		x= file.read()
+	fin.close()
+
+	fin = open('./practica_creativa2/bookinfo/src/productpage/' + fi, 'w')
+	with fin as file:
+		x= x.replace(rep1, rep2)
+		fin.write(x)
+	fin.close()
