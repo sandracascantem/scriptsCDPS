c2077fa6aac4230d01b13c8ae90a4f0246495e7c
sandracascantem <99333138+sandracascantem@users.noreply.github.com>
2023-01-31 14:18:47 +0100

    Update delete.py

--- part3/delete.py
+++ part3/delete.py
@@ -1,1 +1,9 @@
+#!/usr/bin/python3
 
+import os, sys, subprocess, yaml
+from subprocess import call, run
+
+#Borra los contenedores creados
+subprocess.run(["sudo", "docker-compose", "down"])
+#Borra las imagenes creadas
+subprocess.run(["sudo", "docker", "rmi", "35/reviews", "35/ratings", "35/details", "35/productpage"])
