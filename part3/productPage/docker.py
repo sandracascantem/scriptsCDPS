20495d1eb77d089533b8c3fca67b2d790badbc88
sandracascantem <99333138+sandracascantem@users.noreply.github.com>
2023-01-31 18:36:25 +0100

    Update and rename script2.py to docker.py

--- part3/productPage/docker.py
+++ part3/productPage/docker.py
@@ -16,7 +16,9 @@ def arch(fi, rep1, rep2):
 		x= x.replace(rep1, rep2)
 		fin.write(x)
 	fin.close()
+	
 
+	
 #Modificamos el fichero requirements.txt
 arch('requirements.txt', 'urllib3==1.26.5', 'urllib3')
 arch('requirements.txt', 'chardet==3.0.4', 'chardet')
