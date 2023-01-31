4ab20f9081541376b0301dd546f2405769cdb2d3
sandracascantem <99333138+sandracascantem@users.noreply.github.com>
2023-01-31 01:08:28 +0100

    Update script3.py

--- part3/script3.py
+++ part3/script3.py
@@ -5,33 +5,62 @@ from subprocess import call, run
 
 #Funcion para reemplazar dentro del docker-compose.yaml
 def dcompose_ver(fin, version):
-	options = {
+	# dictionary to store environment variables based on version
+	env_vars = {
 		"v1": {"SERVICE_VERSION": "v1", "ENABLE_RATINGS": "false", "STAR_COLOR": "black"},
 		"v2": {"SERVICE_VERSION": "v2", "ENABLE_RATINGS": "true", "STAR_COLOR": "black"},
 		"v3": {"SERVICE_VERSION": "v3", "ENABLE_RATINGS": "true", "STAR_COLOR": "red"}
 	}
-	if version not in options:
-        	print("No se ha seleccionado una versión válida de reviews (v1, v2, v3)")
-        	exit()
+	# check if the specified version is valid
+	if version not in env_vars:
+        	print("Choose a valid version [v1, v2, v3]")
+        	return
 
+	# read the contents of the docker-compose.yaml file
 	with open(fin, "r") as f:
-        	lines = f.readlines()
-	new_lines = []
-	replace_next = False
+        	contents = f.read()
+	
+	# split the contents into lines
+	lines = contents.split("\n")
+	# flag to keep track of the "details" section
+	in_details = False
+	# updated lines with environment variables for "details" section
+	updated_lines = []
+	
 	for line in lines:
-		if "STAR_COLOR" in line:
-			replace_next = True
-		if "SERVICE_VERSION" in line and replace_next:
-			line = re.sub(r"SERVICE_VERSION=\w+", f"SERVICE_VERSION={options[version]['SERVICE_VERSION']}", line)
-			replace_next = False
-		if "ENABLE_RATINGS" in line:
-			line = re.sub(r"ENABLE_RATINGS=\w+", f"ENABLE_RATINGS={options[version]['ENABLE_RATINGS']}", line)
-		if "STAR_COLOR" in line:
-			line = re.sub(r"STAR_COLOR=\w+", f"STAR_COLOR={options[version]['STAR_COLOR']}", line)
-		new_lines.append(line)
+        # check if we're in the "details" section
+        if line.startswith("  details:"):
+		in_details = True
 
+        # if we're in the "details" section, update environment variables
+        if in_details:
+		# check if the line starts with "  - SERVICE_VERSION="
+		if line.startswith("      - SERVICE_VERSION="):
+                	# replace the line with the new value from the dictionary
+                	line = "      - SERVICE_VERSION={}\n".format(env_vars[version]["SERVICE_VERSION"])
+	
+		# check if the line starts with "  - ENABLE_RATINGS="
+		if line.startswith("      - ENABLE_RATINGS="):
+                	# replace the line with the new value from the dictionary
+                	line = "      - ENABLE_RATINGS={}\n".format(env_vars[version]["ENABLE_RATINGS"])
+
+		# check if the line starts with "  - STAR_COLOR="
+		if line.startswith("      - STAR_COLOR="):
+                	# replace the line with the new value from the dictionary
+                	line = "      - STAR_COLOR={}\n".format(env_vars[version]["STAR_COLOR"])
+
+        # add the line to the updated_lines list
+        updated_lines.append(line)
+
+        # check if we've reached the end of the "details" section
+        if line.startswith("    ports:"):
+		in_details = False
+
+	# join the updated lines with newline characters
+	updated_contents = "\n".join(updated_lines)
+	# write the updated contents to the docker-compose.yaml file
 	with open(fin, "w") as f:
-        	f.writelines(new_lines)
+		f.writelines(updated_contents)
 
 #Clonamos la carpeta practica_creativa2 del github
 run(["git", "clone", "https://github.com/CDPS-ETSIT/practica_creativa2"])
