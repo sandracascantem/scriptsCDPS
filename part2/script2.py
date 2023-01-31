d575fec3dfbb360546f20d24a8cae7816c8ff2f8
sandracascantem <99333138+sandracascantem@users.noreply.github.com>
2023-01-31 13:46:42 +0100

    Update script2.py

--- part2/script2.py
+++ part2/script2.py
@@ -1,1 +1,9 @@
 
+from subprocess import call
+#------------------------comandos de instalación-------------------------------------------------------
+call(["git","clone","https://github.com/CDPS-ETSIT/practica_creativa2.git"])
+call(["sudo","apt","update"])
+call(["sudo","apt","install", "-y", "docker.io"])
+
+call(["sudo","docker","build","-t","g14/product-page","."])
+call(["sudo","docker","run", "-d", "--name","g14-productpage","-p9080:9080","g14/product-page"])
