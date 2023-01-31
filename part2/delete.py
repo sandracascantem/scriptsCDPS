9dbad87148ed988a66ff4cbdaef5025cfba8d7bb
sandracascantem <99333138+sandracascantem@users.noreply.github.com>
2023-01-31 17:10:19 +0100

    Update delete.py

--- part2/delete.py
+++ part2/delete.py
@@ -5,5 +5,5 @@ from subprocess import call, run
 
 #Borra el contenedor creado
 subprocess.run(["sudo", "docker", "rm", "35-productpage"])
-#Borra las imagenes creadas
+#Borra la imagen creada
 subprocess.run(["sudo", "docker", "rmi", "35/product-page"])
