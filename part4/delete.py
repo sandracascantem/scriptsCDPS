a07cfbf580c681f41085d962ea9331140d8da3c0
sandracascantem <99333138+sandracascantem@users.noreply.github.com>
2023-01-31 15:41:55 +0100

    Create delete.py

---
+++ part4/delete.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import os, sys, subprocess, yaml
+from subprocess import call, run
+
+#Construimos los servicios de kubernetes
+subprocess.run(["kubectl", "delete", "-f", "productPage/productpage.yaml"])
+subprocess.run(["kubectl", "delete", "-f", "details/details.yaml"])
+subprocess.run(["kubectl", "delete", "-f", "ratings/ratings.yaml"])
+subprocess.run(["kubectl", "delete", "-f", "reviews/reviews-svc.yaml"])
+subprocess.run(["kubectl", "delete", "-f", "reviews/reviews-v1-deployment.yaml"])
+subprocess.run(["kubectl", "delete", "-f", "reviews/reviews-v2-deployment.yaml"])
+subprocess.run(["kubectl", "delete", "-f", "reviews/reviews-v3-deployment.yaml"])
+  
