7e24b3d971a417e813ecaa99f46e5e5206125616
sandracascantem <99333138+sandracascantem@users.noreply.github.com>
2023-01-31 18:37:49 +0100

    Update docker.py

--- part3/productPage/docker.py
+++ part3/productPage/docker.py
@@ -17,8 +17,9 @@ def arch(fi, rep1, rep2):
 		fin.write(x)
 	fin.close()
 	
+#Clonamos la carpeta practica_creativa2 del github
+subprocess.run(["git", "clone", "https://github.com/CDPS-ETSIT/practica_creativa2"])
 
-	
 #Modificamos el fichero requirements.txt
 arch('requirements.txt', 'urllib3==1.26.5', 'urllib3')
 arch('requirements.txt', 'chardet==3.0.4', 'chardet')
@@ -29,6 +30,8 @@ subprocess.run(["apt-get", "install", "python3-pip"])
 #Instalamos dependencias de requirements.txt con pip
 subprocess.run(["pip3", "install", '-r', './practica_creativa2/bookinfo/src/productpage/requirements.txt'])
 
-#Capturamos la variable GROUP_NUMBER definida en el Dockerfile
+#Extraemos la variable de entorno (definida en el Dockerfile) y guardarla aqui
 title = os.environ.get('GROUP_NUMBER')
+
+#Modificamos productpage.html para cambiar el titulo de la app por la variable de entorno
 arch('templates/productpage.html', 'Simple Bookstore App', title)
